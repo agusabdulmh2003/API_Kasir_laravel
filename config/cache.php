fdac901b5f79233bf6946e69353d0b555a4d2448
agusabdulmh2003 <21sa1228@mhs.amikompurwokerto.ac.id>
2025-01-26 00:21:21 +0700

    api pennjualan

--- config/cache.php
+++ config/cache.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('CACHE_STORE', 'database'),
+    'default' => env('CACHE_DRIVER', 'file'), // Default driver diubah ke file untuk menghindari error awal
 
     /*
     |--------------------------------------------------------------------------
@@ -40,16 +40,13 @@ return [
 
         'database' => [
             'driver' => 'database',
-            'connection' => env('DB_CACHE_CONNECTION'),
-            'table' => env('DB_CACHE_TABLE', 'cache'),
-            'lock_connection' => env('DB_CACHE_LOCK_CONNECTION'),
-            'lock_table' => env('DB_CACHE_LOCK_TABLE'),
+            'table' => 'cache', // Nama tabel cache
+            'connection' => null, // Gunakan koneksi default
         ],
 
         'file' => [
             'driver' => 'file',
             'path' => storage_path('framework/cache/data'),
-            'lock_path' => storage_path('framework/cache/data'),
         ],
 
         'memcached' => [
@@ -73,8 +70,7 @@ return [
 
         'redis' => [
             'driver' => 'redis',
-            'connection' => env('REDIS_CACHE_CONNECTION', 'cache'),
-            'lock_connection' => env('REDIS_CACHE_LOCK_CONNECTION', 'default'),
+            'connection' => 'default',
         ],
 
         'dynamodb' => [
